b944e1fcb90d7a41b91093e90c0b19e1b010224b
TrevorZylks <trevorzylks@gmail.com>
2021-07-10 16:31:23 -0700

    commit tree 9751a171a66740ce594961ba9cf2712861c24ddb

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code here
+
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.soup = soup;
+        this.salad = salad;
+        this.drink = drink;
+    }
+}
+
+let lunch = new Lunch('side salad', 'side salad', 'Pepsi')
+
+console.log(lunch.soup)
+
+class Dinner { 
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = dessert;
+    }
+}
+
+let newdinner = new Dinner('balsamic', 'consomme', 'filet mignon', 'cheesecake')
+
+console.log(newdinner)
